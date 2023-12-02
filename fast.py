429886dba7ca347ea45a08c03d4e6fe7168f7ee7
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-12-02 23:03:40 +0200

    something

--- fast.py
+++ fast.py
@@ -1,5 +1,3 @@
-import os
-from passlib.hash import md5_crypt
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI, APIRouter, Request
 import psycopg2
@@ -8,15 +6,15 @@ import subprocess
 app = FastAPI()
 router = APIRouter(prefix="/api")
 
-conn=psycopg2.connect(database="test",
+conn=psycopg2.connect(database="blackmetal",
                         host="localhost",
-                        user="salla",
-                        password="iluvkoji",
+                        user="bm_admin",
+                        password="18cba9cd-0776-4f09-9c0e-41d2937fab2b",
                         port=5432) 
 @router.get("/")
 async def get_what():
     cursor = conn.cursor()
-    command = "select * from what;"
+    command = "select * from albums;"
     cursor.execute(command)
     data = cursor.fetchall()
     response = JSONResponse({"detail":data},200)        
@@ -25,14 +23,11 @@ async def get_what():
 @router.post("/register")
 async def register(request:Request):
     content = await request.json()
-    #hashed_pwd = md5_crypt.hash(content["password"])
-    command1 = 'sudo sh -c "echo -n "%s:" >> /etc/nginx/.pwd"' % content["username"]
-    command2 = 'sudo sh -c "openssl passwd -apr1 "%s" >> /etc/nginx/.pwd"' % content["password"]
-    subprocess.run([command1],shell=True)
-    subprocess.run([command2],shell=True)
-    #path = "/etc/nginx/.pwd"
-    #pwd_file = open(path, "a")
-    #pwd_file.write('%s:%s' % (content["username"], hashed_pwd))
+    openssl_cmd = 'openssl passwd -apr1 "%s"' % content["password"] 
+    hashed_pwd = subprocess.run(openssl_cmd,shell=True,capture_output=True,text = True).stdout.replace("\n","")
+    echo_string = '%s:%s' % (content["username"],hashed_pwd)
+    pwd_string = "echo '%s' >> /etc/nginx/.pwd" % echo_string
+    subprocess.run(pwd_string,shell=True)
     response = JSONResponse({"detail":"data received successfull"},200)
     return response
 
